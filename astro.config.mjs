102861fe9d5171d3e6dab3282c15798ddb6942e7
jd <jhon21720@gmail.com>
2025-04-29 22:32:11 -0300

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output : "server",
-  adapter: netlify()
+  output : 'server',
+  adapter : netlify()
+  
 });
