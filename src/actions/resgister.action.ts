f55c1e04f761b532be617303f9a71c978f8141ee
jd <jhon21720@gmail.com>
2025-11-07 16:48:23 -0300

    incluye resgistro de usuario a la base de datos

--- src/actions/resgister.action.ts
+++ src/actions/resgister.action.ts
@@ -1,30 +1,69 @@
-import { defineAction } from "astro:actions";
+import { ActionError, defineAction } from "astro:actions";
 import { z } from "astro:schema";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, type AuthError } from "firebase/auth";
 import { firebase } from "../firebase/config";
+import { setDoc, doc } from "firebase/firestore";
 
 export const registerUser = defineAction({
-    accept: "form",
-    input: z.object({
-        nombre: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6),
+  accept: "form",
+  input: z.object({
+    email: z.string({
+      invalid_type_error: "EMAIL : NO DEBE ESTAR VACIO"
+    }).email(),
+    password: z.string().min(3),
+    habilitacion: z.string({
+      invalid_type_error: "HABILITACION : NO DEBE ESTAR VACIO"
     }),
-    handler: async ({ nombre, email, password }) => {
+    nombre: z.string({
+      invalid_type_error: "NOMBRE : NO DEBE ESTAR VACIO"
+    }).refine(
+      (val) => !/\d/.test(val),
+      "NOMBRE : SOLO ADMITE LETRAS"
+    ),
+    medico: z.string({
+      invalid_type_error: 'MEDICO: NO DEBE ESTAR VACIO'
+    }),
+    salvavida: z.string({
+      invalid_type_error: "SALVAVIDA : NO DEBE ESTAR VACIO"
+    }),
+    id: z.number(),
+  }),
 
-        //creacion de usuario
-        
-             await createUserWithEmailAndPassword(
-                firebase.auth, email, password
-            )
-            
+  handler: async ({ nombre, email, password, habilitacion, medico, salvavida, id }) => {
 
+    /* console.log({nombre, email, password, habilitacion, medico, salvavida, id})
+     return true */
 
-       
-       //return {ok:true , msg:'todo bien por aca'}
-        
+    try {
+      const userId = await createUserWithEmailAndPassword(
+        firebase.auth, email, password)
+      const user = userId.user;
+
+      await setDoc(doc(firebase.db, 'userscp', user.uid), {
+        nombre: nombre,
+        email: email,
+        habilitacion: habilitacion,
+        medico: medico,
+        salvavida: salvavida,
+        id: id,
+        uid: user.uid
+      });
+
+    } catch (error) {
+
+      const firebaseError = error as AuthError;
+      if (firebaseError.code == 'auth/email-already-in-use') {
+        throw new Error('EL CORREO YA EXISTE')
+      }
     }
-
-
+    return
+  }
 
 })
+
+
+
+
+
+
+
